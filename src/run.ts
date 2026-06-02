d868da3c7b219a608961268e79f6b69f9bc544cb
MrSIHAB <MrSIHAB@hotmail.com>
2026-06-02 10:59:02 +0600

    add docs and improve logics

--- src/run.ts
+++ src/run.ts
@@ -1,17 +1,29 @@
+/**
+ * First parameter(`element`) is your entire website built with the library. It accepts an HTML Element or a list of HTML Elements. 
+ * The second parameter is optional and is the id of the root element where you want to mount your website. If you don't provide a 
+ * root element.
+ * 
+ * In case you have a dedicated root element in your HTML file, you can pass its id as the second parameter. 
+ * For example, if your HTML file has a div with id **root**, you can call `Run(App, "root")` to mount your website inside that div. 
+ * If you don't provide a root element, the library will default to mounting your website directly on the body of the document.
+ * 
+ * @param element HTMLElement
+ * @param root string
+ * 
+ _* Example usage:_
+ * ```typescript
+ * import { Run } from "vanilla-ui";
+ * import App from "./src/app.ts";
+ * 
+ * Run(App, "root");
+ * ```
+ */
 export const Run = (
   element: HTMLElement | HTMLElement[],
   root?: string,
 ): void => {
-  // Reset default body styles
-  const body = document.body;
-  body.style.margin = "0";
-  body.style.padding = "0";
-  body.style.boxSizing = "border-box";
-
-  const rootElement = root ? document.querySelector(root) : body;
-  if (!rootElement) {
-    throw new Error(`Root element with id "#${root}" not found.`);
-  }
+  const rootElement = root ? document.getElementById(root) : document.body;
+  if (!rootElement) throw new Error(`Root element "#${root}" not found.`); // Undefined root check
 
   if (Array.isArray(element)) {
     element.forEach((elem) => {
